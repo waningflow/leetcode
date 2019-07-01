a20770700e604fd65850c30f0d408d44ca24cc02
waningflow <jack1995woods@gmail.com>
2019-07-01 13:18:16 +0800

    Update 172.js

--- easy/172.js
+++ easy/172.js
@@ -2,4 +2,11 @@
  * @param {number} n
  * @return {number}
  */
-var trailingZeroes = function(n) {}
+var trailingZeroes = function(n) {
+  let count = 0
+  while (n >= 5) {
+    count += Math.floor(n / 5)
+    n = Math.floor(n / 5)
+  }
+  return count
+}
