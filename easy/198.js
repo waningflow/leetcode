b526d18590450d2615233551feec51bcbea2cedf
Jack Woods <jack1995woods@gmail.com>
2019-08-16 00:29:01 +0800

    Update 198.js

--- easy/198.js
+++ easy/198.js
@@ -1,1 +1,14 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  let a = 0;
+  let b = 0;
+  for(let i=0;i<nums.length;i++){
+      let t = b;
+      b = Math.max(a + nums[i], b)
+      a = t
+  }
+  return b
+};
