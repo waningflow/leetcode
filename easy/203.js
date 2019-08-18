4ecda31006cb661206b4dfea91a9636882ad5a1e
waningflow <jack1995woods@gmail.com>
2019-08-18 18:03:12 +0800

    Update 203.js

--- easy/203.js
+++ easy/203.js
@@ -1,3 +1,14 @@
+function ListNode(val) {
+  this.val = val
+  this.next = null
+}
+let hd = new ListNode(1)
+let current = hd
+current.next = new ListNode(2)
+current = current.next
+current.next = new ListNode(6)
+current = current.next
+current.next = new ListNode(3)
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -11,5 +22,18 @@
  * @return {ListNode}
  */
 var removeElements = function(head, val) {
-    
-};
+  let res = {}
+  let current = head
+  let cRes = res
+  while (current) {
+    if (current.val !== val) {
+      cRes.next = current
+      cRes = cRes.next
+    }
+    current = current.next
+  }
+  cRes.next = null
+  return res.next
+}
+
+console.log(removeElements(hd, 6))
