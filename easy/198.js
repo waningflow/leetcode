49a3c0efa63495504e77da0a2b191262a501f8af
waningflow <jack1995woods@gmail.com>
2019-08-18 16:51:58 +0800

    Update 198.js

--- easy/198.js
+++ easy/198.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var rob = function(nums) {
-  let a = 0;
-  let b = 0;
-  for(let i=0;i<nums.length;i++){
-      let t = b;
-      b = Math.max(a + nums[i], b)
-      a = t
+  let a = 0
+  let b = 0
+  for (let i = 0; i < nums.length; i++) {
+    let t = b
+    b = Math.max(a + nums[i], b)
+    a = t
   }
   return b
-};
+}
