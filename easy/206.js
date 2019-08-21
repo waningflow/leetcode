5fd8c3285db7645f9b3d9bbce86b4065219bcb2c
waningflow <jack1995woods@gmail.com>
2019-08-21 23:28:39 +0800

    Update 206.js

--- easy/206.js
+++ easy/206.js
@@ -10,5 +10,13 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    
-};
+  let cur = head
+  let pre = null
+  while (cur) {
+    let next = cur.next
+    cur.next = pre
+    pre = cur
+    cur = next
+  }
+  return pre
+}
