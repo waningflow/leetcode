fe6ef8ebe65a3a2ce352d1e1cc8a888d5e4619bb
waningflow <jack1995woods@gmail.com>
2019-09-10 22:49:06 +0800

    Update 226.js

--- easy/226.js
+++ easy/226.js
@@ -10,5 +10,12 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    
-};
+  if (!root) {
+    return root
+  }
+  let left = root.left
+  let right = root.right
+  root.left = invertTree(right)
+  root.right = invertTree(left)
+  return root
+}
