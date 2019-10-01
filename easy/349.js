55edeacdf0788b7b82df8aca033154957385e56e
Jack Woods <jack1995woods@gmail.com>
2019-10-01 21:44:14 +0800

    Update 349.js

--- easy/349.js
+++ easy/349.js
@@ -4,5 +4,17 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    
+  let nums = {}
+  let rtn = []
+  for(let i=0;i<nums1.length;i++){
+      nums[nums1[i]] = 1
+  }
+  for(let i=0;i<nums2.length;i++){
+    const num = nums2[i]
+    if(nums[num]){
+        rtn.push(num)
+        nums[num] = 0
+    }
+  }
+  return rtn
 };
