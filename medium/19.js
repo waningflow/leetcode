4495b032e7ad96b9f08e28ef6e99442ea651029e
waningflow <jack1995woods@gmail.com>
2019-11-28 23:52:48 +0800

    Update 19.js

--- medium/19.js
+++ medium/19.js
@@ -12,8 +12,24 @@ const { initListNodeFromArr } = require('../util.js')
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {}
+var removeNthFromEnd = function(head, n) {
+  // 预留一个节点（n与链表长度相同时）
+  let c1 = { next: head },
+    c2 = { next: head }
+  let nhead = c1
+  while (n > 0) {
+    c2 = c2.next
+    n -= 1
+  }
+  while (c2.next !== null) {
+    c1 = c1.next
+    c2 = c2.next
+  }
+  // 判空（n为0时）
+  if (c1.next) c1.next = c1.next.next
+  return nhead.next
+}
 
 // test
 let h = initListNodeFromArr([1, 2, 3, 4, 5])
-removeNthFromEnd(h, 2)
+let r = removeNthFromEnd(h, 2)
