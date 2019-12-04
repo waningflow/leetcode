8cca674c78a8b91951ee659c0fe272568e47527f
waningflow <jack1995woods@gmail.com>
2019-12-04 19:12:29 +0800

    Update 821.js

--- easy/821.js
+++ easy/821.js
@@ -4,5 +4,25 @@
  * @return {number[]}
  */
 var shortestToChar = function(S, C) {
-    
-};
+  const rtn = []
+  let index = -Infinity
+  for (let i = 0; i < S.length; i++) {
+    if (S[i] === C) {
+      index = i
+    }
+    rtn.push(i - index)
+  }
+  index = Infinity
+  for (let i = S.length - 1; i >= 0; i--) {
+    if (S[i] === C) {
+      index = i
+    }
+    rtn[i] = Math.min(index - i, rtn[i])
+  }
+  return rtn
+}
+
+// test
+const S = 'loveleetcode',
+  C = 'e'
+console.log(shortestToChar(S, C))
