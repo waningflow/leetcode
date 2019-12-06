02fcc4a5033f7ee5f66e9e7f5bd0f9bf9692e836
waningflow <jack1995woods@gmail.com>
2019-12-06 21:43:54 +0800

    Update 6.js

--- medium/6.js
+++ medium/6.js
@@ -3,4 +3,38 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, numRows) {}
+var convert = function(s, numRows) {
+  if (numRows === 1) {
+    return s
+  }
+  const group = 2 * numRows - 2
+  let rtn = []
+  for (let i = 0; i < numRows; i++) {
+    rtn[i] = []
+  }
+  while (s.length) {
+    let str = s.slice(0, group)
+    s = s.slice(group)
+    let index = 0,
+      direction = 1
+    for (let i = 0; i < str.length; i++) {
+      rtn[index].push(str[i])
+      index += direction
+      if (index >= numRows) {
+        index = numRows - 2
+        direction = -1
+      }
+    }
+  }
+  let rtnStr = ''
+  for (let i = 0; i < numRows; i++) {
+    rtnStr += rtn[i].join('')
+  }
+  return rtnStr
+}
+
+// test
+const s = 'PAYPALISHIRING',
+  numRows = 4
+
+console.log(convert(s, numRows))
