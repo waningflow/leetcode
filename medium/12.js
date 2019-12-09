28d9b3c3d565a2f6442315ae09b2c8a1070cfc55
waningflow <jack1995woods@gmail.com>
2019-12-09 23:07:35 +0800

    Update 12.js

--- medium/12.js
+++ medium/12.js
@@ -3,5 +3,16 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    
-};
+  let rtn = ''
+  var N = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  var R = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+  for (let i = 0; i < N.length; i++) {
+    while (num >= N[i]) {
+      rtn += R[i]
+      num -= N[i]
+    }
+  }
+  return rtn
+}
+
+console.log(intToRoman(1994))
