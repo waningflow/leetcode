77aa01f095a3a6cb9033e30f6339136d86bf7232
waningflow <jack1995woods@gmail.com>
2019-12-15 23:21:24 +0800

    Update 390.js

--- medium/390.js
+++ medium/390.js
@@ -3,5 +3,27 @@
  * @return {number}
  */
 var lastRemaining = function(n) {
-    
-};
+  let a = []
+  for (let i = 0; i < n; i++) {
+    a.push(i + 1)
+  }
+  let right = true
+  while (a.length > 1) {
+    let b = a
+    a = []
+    for (let i = 0; i < b.length; i++) {
+      if (i % 2 === 1) {
+        if (right) {
+          a.push(b[i])
+        } else {
+          a.unshift(b[b.length - 1 - i])
+        }
+      }
+    }
+    right = !right
+  }
+  return a[0]
+}
+
+// test
+console.log(lastRemaining(10000000))
